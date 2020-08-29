9c0b715da6e1466684113209be8ec2bc9ca1390d
kawaharakenta <kawaharakenta@team-lab.com>
2020-08-30 01:18:44 +0900

    first commit

---
+++ src/main/kotlin/com/example/trainingapikotlin/TrainingapikotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.example.trainingapikotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TrainingapikotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<TrainingapikotlinApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/trainingapikotlin/TrainingapikotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.trainingapikotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TrainingapikotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
